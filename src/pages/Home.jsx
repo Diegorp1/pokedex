9277a36f9421ddcfa97b8040b45feff4ecb6ca30
Diego Pacheco <diegorp__@hotmail.com>
2023-04-04 15:51:41 -0300

    adicionando novo código

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import PokemonCard from "../components/PokemonCard";
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Pagina Inicial</h1>
+        <PokemonCard/>
+    </div>
+  )
+  }
+
+  export default Home;
